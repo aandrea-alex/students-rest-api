da148d3e6ad674758deaf12734088cf634414caf
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-11-20 22:40:27 +0100

    create http-server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const message = "Hello world!";
+import express from 'express';
 
-console.log(message);
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
