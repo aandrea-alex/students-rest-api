8e907ee71e51cea650e11834ea2914c5c95dcb3d
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-11-20 22:54:27 +0100

    create files structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 
 app.get('/', (req, res) => {
   res.json({
