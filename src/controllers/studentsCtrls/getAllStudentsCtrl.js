6d9f3a6e359196d3e9334fcc17884a913d0f2292
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-11-26 14:53:27 +0100

    added sort and filter

--- src/controllers/studentsCtrls/getAllStudentsCtrl.js
+++ src/controllers/studentsCtrls/getAllStudentsCtrl.js
@@ -1,5 +1,7 @@
 import { getAllStudents } from '../../services/students/getAllstudents.js';
 import { parsePaginationParams } from '../../utils/parsePaginationParams.js';
+import { parseSortParams } from '../../utils/parseSortParams.js';
+import { parseFilterParams } from '../../utils/parseFilterParams.js';
 
 // export const getAllStudentsCtrl = async (req, res, next) => {
 //   const studs = await getAllStudents();
@@ -12,10 +14,15 @@ import { parsePaginationParams } from '../../utils/parsePaginationParams.js';
 
 export const getAllStudentsCtrl = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  console.log(page, perPage);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
   const students = await getAllStudents({
     page,
     perPage,
+    sortBy,
+    sortOrder,
+    filter,
   });
 
   res.json({
--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -1,4 +1,14 @@
-import { parseNumber } from './parseNumber.js';
+const parseNumber = (number, defaultValue) => {
+  const isString = typeof number === 'string';
+  if (!isString) return defaultValue;
+
+  const parsedNumber = parseInt(number);
+  if (Number.isNaN(parsedNumber)) {
+    return defaultValue;
+  }
+
+  return parsedNumber;
+};
 
 export const parsePaginationParams = (query) => {
   const { page, perPage } = query;
